13377428d78e177409d1d4363a490f84a7285b7f
Eunice Ogunrinde <eunice@gladepay.com>
2021-01-03 23:04:09 +0100

    Second commit
    Signed-off-by: Eunice Ogunrinde <eunice@gladepay.com>

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="section section-lg section-shaped overflow-hidden my-0">
+        <b-carousel id="carousel1" controls indicators>
+            <!-- Text slides with image -->
+            <b-carousel-slide img-src="img/theme/img-1-1200x1000.jpg"></b-carousel-slide>
+            <b-carousel-slide img-src="img/theme/img-2-1200x1000.jpg"></b-carousel-slide>
+        </b-carousel>
+    </section>
+</template>
+<script>
+import { BCarousel } from "bootstrap-vue/esm/components/carousel/carousel";
+import { BCarouselSlide } from "bootstrap-vue/esm/components/carousel/carousel-slide";
+
+export default {
+  components: {
+    BCarousel,
+    BCarouselSlide
+  }
+};
+</script>
+<style>
+</style>
